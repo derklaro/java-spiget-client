648625237dd5799250bcd585502baf8b85fa5369
derklaro <git@derklaro.dev>
2022-01-17 15:21:00 +0100

    build: configure publish to sonatype repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,14 +25,15 @@
 import org.cadixdev.gradle.licenser.LicenseExtension
 
 plugins {
+  id("org.cadixdev.licenser") version "0.6.1"
   id("com.github.ben-manes.versions") version "0.41.0"
-  id("org.cadixdev.licenser") version "0.6.1" apply false
+  id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 defaultTasks("build", "checkLicenses", "test", "jar")
 
 allprojects {
-  group = "dev.derklaro"
+  group = "dev.derklaro.spiget"
   version = "1.0.0-SNAPSHOT"
 
   repositories {
@@ -41,6 +42,7 @@ allprojects {
 }
 
 subprojects {
+  apply(plugin = "signing")
   apply(plugin = "checkstyle")
   apply(plugin = "java-library")
   apply(plugin = "maven-publish")
@@ -77,12 +79,90 @@ subprojects {
     configFile = rootProject.file("checkstyle.xml")
   }
 
+  tasks.withType<Sign> {
+    onlyIf {
+      !project.rootProject.version.toString().endsWith("-SNAPSHOT")
+    }
+  }
+
   extensions.configure<LicenseExtension> {
     include("**/*.java")
     header(rootProject.file("license_header.txt"))
   }
 
+  extensions.configure<JavaPluginExtension> {
+    withSourcesJar()
+    withJavadocJar()
+  }
+
   extensions.configure<CheckstyleExtension> {
     toolVersion = "9.2.1"
   }
+
+  extensions.configure<PublishingExtension> {
+    publications {
+      create("library", MavenPublication::class.java) {
+        from(project.components.getByName("java"))
+
+        pom {
+          name.set(project.name)
+          url.set("https://github.com/derklaro/java-spiget-client")
+
+          licenses {
+            license {
+              name.set("MIT License")
+              url.set("https://opensource.org/licenses/MIT")
+            }
+          }
+
+          developers {
+            developer {
+              name.set("Pasqual Koschmieder")
+              email.set("git@derklaro.dev")
+            }
+          }
+
+          scm {
+            url.set("https://github.com/derklaro/java-spiget-client")
+            connection.set("https://github.com/derklaro/java-spiget-client.git")
+          }
+
+          issueManagement {
+            system.set("GitHub Issues")
+            url.set("https://github.com/derklaro/java-spiget-client/issues")
+          }
+
+          withXml {
+            val repositories = asNode().appendNode("repositories")
+            project.repositories.forEach {
+              if (it is MavenArtifactRepository && it.url.toString().startsWith("https://")) {
+                val repo = repositories.appendNode("repository")
+                repo.appendNode("id", it.name)
+                repo.appendNode("url", it.url.toString())
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  extensions.configure<SigningExtension> {
+    useGpgCmd()
+    sign(extensions.getByType(PublishingExtension::class.java).publications["library"])
+  }
+}
+
+nexusPublishing {
+  repositories {
+    sonatype {
+      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+      username.set(project.findProperty("ossrhUsername")?.toString() ?: "")
+      password.set(project.findProperty("ossrhPassword")?.toString() ?: "")
+    }
+  }
+
+  useStaging.set(!project.rootProject.version.toString().endsWith("-SNAPSHOT"))
 }
